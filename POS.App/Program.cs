0ce78dda74be7d2565a24b7e9c61b5c7a2ef49f2
Quang Phan <79438276+quangphantally@users.noreply.github.com>
2023-10-10 10:36:38 +0700

    add forms

--- POS.App/Program.cs
+++ POS.App/Program.cs
@@ -1,3 +1,5 @@
+using POS.App.Forms;
+
 namespace POS.App
 {
     internal static class Program
@@ -11,7 +13,7 @@ namespace POS.App
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new ProductListForm());
         }
     }
 }
