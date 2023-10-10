357cee7f4f91bc17a73365ec5716bdfd637fde18
Quang Phan <79438276+quangphantally@users.noreply.github.com>
2023-10-10 10:25:00 +0700

    save work

---
+++ Pos.App/Form2.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Pos.App
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
+            listView1.Location = new Point(30, 118);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(1068, 463);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            listView1.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Name";
+            columnHeader1.Width = 350;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Date modified";
+            columnHeader2.Width = 120;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Type";
+            columnHeader3.Width = 120;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Size";
+            columnHeader4.Width = 100;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(30, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(752, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(804, 12);
+            button1.Name = "button1";
+            button1.Size = new Size(94, 29);
+            button1.TabIndex = 2;
+            button1.Text = "Explore";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1262, 625);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(listView1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private TextBox textBox1;
+        private Button button1;
+    }
+}
---
+++ Pos.App/Form2.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pos.App
+{
+    public enum SortDirection
+    {
+        Ascending,
+        Descending,
+    }
+
+    public partial class Form2 : Form
+    {
+        private List<FileRecord> list = null;
+        public Form2()
+        {
+            InitializeComponent();
+            listView1.ColumnClick += ListView1_ColumnClick;
+        }
+
+        private SortDirection[] columns = new SortDirection[]
+        {
+            SortDirection.Descending,
+            SortDirection.Ascending,
+            SortDirection.Ascending,
+            SortDirection.Ascending
+        };
+
+        private void ListView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            columns[e.Column] = columns[e.Column] ==  SortDirection.Descending ? SortDirection.Ascending : SortDirection.Descending;
+
+                var asc = columns[e.Column] == SortDirection.Ascending;
+
+            if (e.Column == 0)
+            {
+                if (asc)
+                {
+                    list = list.OrderBy(x => x.Name).ToList();
+                }
+                else
+                {
+                    list = list.OrderByDescending(x => x.Name).ToList();
+                }
+            }
+            else if (e.Column == 1)
+            {
+                // sort modified
+                if (asc)
+                {
+                    list = list.OrderBy(x => x.ModifiedDate).ToList();
+                }
+                else
+                {
+                    list = list.OrderByDescending(x => x.ModifiedDate).ToList();
+                }
+            }
+            else if (e.Column == 2)
+            {
+                // sort modified
+                if (asc)
+                {
+                    list = list.OrderBy(x => x.Type).ToList();
+                }
+                else
+                {
+                    list = list.OrderByDescending(x => x.Type).ToList();
+                }
+            }
+            else if (e.Column == 3)
+            {
+                // sort modified
+                if (asc)
+                {
+                    list = list.OrderBy(x => x.Size).ToList();
+                }
+                else
+                {
+                    list = list.OrderByDescending(x => x.Size).ToList();
+                }
+            }
+
+            RenderListView(list);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var path = textBox1.Text;
+            if (string.IsNullOrWhiteSpace(path))
+            {
+                return;
+            }
+
+            if (!Path.Exists(path))
+            {
+                MessageBox.Show("Invalid path");
+                return;
+            }
+
+            // ok
+            var directoryInfo = new DirectoryInfo(path);
+            var files = directoryInfo.EnumerateFiles().Select(info => new FileRecord
+            {
+                Name = info.Name,
+                ModifiedDate = info.LastWriteTime,
+                Type = info.Extension,
+                Unit = "KB",
+                Size = info.Length / 1024,
+            }).ToList();
+
+            var directories = directoryInfo.EnumerateDirectories().Select(x => new FileRecord
+            {
+                Name = x.Name,
+                ModifiedDate = x.LastWriteTime,
+                Type = "File folder",
+            }).ToList();
+
+            files.AddRange(directories);
+            list = new List<FileRecord>(files);
+
+
+            RenderListView(files);
+        }
+
+        private void RenderListView(List<FileRecord> records)
+        {
+
+            listView1.Items.Clear();
+            foreach (var r in records)
+            {
+                var item = new ListViewItem(r.Name);
+                item.SubItems.Add(r.ModifiedDate.ToString("dd/MM/yyy hh:mm tt"));
+                item.SubItems.Add(r.Type);
+                item.SubItems.Add(r.SizeDisplay);
+
+                listView1.Items.Add(item);
+            }
+        }
+    }
+
+    public class FileRecord
+    {
+        public string Name { get; set; }
+        public DateTime ModifiedDate { get; set; }
+        public string Type { get; set; }
+        public double? Size { get; set; }
+        public string Unit { get; set; }
+        public string SizeDisplay => Size > 0 ? $"{Size} {Unit}" : "";
+    }
+}
