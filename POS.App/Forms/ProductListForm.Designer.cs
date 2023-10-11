ed5dcd23189da155f8d5e74618cc7dcfa6b027ba
Quang Phan <79438276+quangphantally@users.noreply.github.com>
2023-10-11 15:22:02 +0700

    reset code

--- POS.App/Forms/ProductListForm.Designer.cs
+++ POS.App/Forms/ProductListForm.Designer.cs
@@ -29,6 +29,13 @@
         private void InitializeComponent()
         {
             lvProducts = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            columnHeader5 = new ColumnHeader();
+            columnHeader6 = new ColumnHeader();
+            columnHeader7 = new ColumnHeader();
             txtSearch = new TextBox();
             btnSearch = new Button();
             btnClear = new Button();
@@ -43,11 +50,50 @@
             // lvProducts
             // 
             lvProducts.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lvProducts.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4, columnHeader5, columnHeader6, columnHeader7 });
+            lvProducts.FullRowSelect = true;
+            lvProducts.GridLines = true;
             lvProducts.Location = new Point(12, 80);
+            lvProducts.MultiSelect = false;
             lvProducts.Name = "lvProducts";
-            lvProducts.Size = new Size(776, 337);
+            lvProducts.Size = new Size(888, 337);
             lvProducts.TabIndex = 0;
             lvProducts.UseCompatibleStateImageBehavior = false;
+            lvProducts.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "Id";
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "Product name";
+            columnHeader2.Width = 300;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "Unit price";
+            columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Stock unit";
+            columnHeader4.Width = 90;
+            // 
+            // columnHeader5
+            // 
+            columnHeader5.Text = "Order unit";
+            columnHeader5.Width = 90;
+            // 
+            // columnHeader6
+            // 
+            columnHeader6.Text = "Category";
+            columnHeader6.Width = 120;
+            // 
+            // columnHeader7
+            // 
+            columnHeader7.Text = "Supplier";
+            columnHeader7.Width = 120;
             // 
             // txtSearch
             // 
@@ -77,7 +123,7 @@
             // btnAdd
             // 
             btnAdd.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            btnAdd.Location = new Point(694, 10);
+            btnAdd.Location = new Point(806, 10);
             btnAdd.Name = "btnAdd";
             btnAdd.Size = new Size(94, 29);
             btnAdd.TabIndex = 4;
@@ -90,7 +136,7 @@
             statusStrip1.Items.AddRange(new ToolStripItem[] { lblStatus });
             statusStrip1.Location = new Point(0, 428);
             statusStrip1.Name = "statusStrip1";
-            statusStrip1.Size = new Size(800, 22);
+            statusStrip1.Size = new Size(912, 22);
             statusStrip1.TabIndex = 5;
             statusStrip1.Text = "statusStrip1";
             // 
@@ -102,7 +148,7 @@
             // button1
             // 
             button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            button1.Location = new Point(694, 45);
+            button1.Location = new Point(806, 45);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 6;
@@ -112,7 +158,7 @@
             // button2
             // 
             button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            button2.Location = new Point(594, 45);
+            button2.Location = new Point(706, 45);
             button2.Name = "button2";
             button2.Size = new Size(94, 29);
             button2.TabIndex = 7;
@@ -123,7 +169,7 @@
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(912, 450);
             Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(statusStrip1);
@@ -153,5 +199,12 @@
         private ToolStripStatusLabel lblStatus;
         private Button button1;
         private Button button2;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
     }
 }
--- POS.App/Forms/ProductListForm.cs
+++ POS.App/Forms/ProductListForm.cs
@@ -11,69 +11,10 @@ namespace POS.App.Forms
         public ProductListForm()
         {
             InitializeComponent();
-            AddListViewHeaders();
-            ConfigureListView();
         }
 
         private void ProductListForm_Load(object sender, EventArgs e)
         {
-            LoadProductList();
-        }
-
-        private void LoadProductList()
-        {
-            var products = productService.GetProductList();
-            RenderProductListView(products);
-        }
-
-        private void RenderProductListView(List<Product> products)
-        {
-            var items = products.Select(p =>
-            {
-                var item = new ListViewItem(p.Id.ToString());
-                item.SubItems.Add(p.ProductName);
-                item.SubItems.Add(p.UnitPrice.ToString());
-                item.SubItems.Add(p.UnitsInStock.ToString());
-                item.SubItems.Add(p.UnitsOnOrder.ToString());
-                item.SubItems.Add(p.CategoryName);
-                item.SubItems.Add(p.SupplierName);
-                item.Tag = p;
-
-                return item;
-            }).ToArray();
-
-            lvProducts.Items.Clear();
-            lvProducts.Items.AddRange(items);
-        }
-
-        private void AddListViewHeaders()
-        {
-            MakeColumn("Id");
-            MakeColumn("Product name", 300);
-            MakeColumn("Price", 100, HorizontalAlignment.Right);
-            MakeColumn("Stock unit", 100, HorizontalAlignment.Right);
-            MakeColumn("Order unit", 100, HorizontalAlignment.Right);
-            MakeColumn("Category", 200);
-            MakeColumn("Supplier", 200);
-        }
-
-        private void MakeColumn(string name, int width = 60, HorizontalAlignment textAlign = HorizontalAlignment.Left)
-        {
-            var col = new ColumnHeader
-            {
-                Text = name,
-                Width = width,
-                TextAlign = textAlign,
-            };
-            lvProducts.Columns.Add(col);
-        }
-
-        private void ConfigureListView()
-        {
-            lvProducts.View = View.Details;
-            lvProducts.MultiSelect = false;
-            lvProducts.GridLines = true;
-            lvProducts.FullRowSelect = true;
         }
     }
 }
